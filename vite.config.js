0693c7fea02e1e2645bc7c051edd94bde98e8394
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-04-13 13:33:54 +0100

    more SEO optimization (reconfigured vite.config.js to generate manifest at every build)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __dirname = resolve(fileURLToPath(import.meta.url), '..')
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    manifest: true,   // generate manifest.json
+    rollupOptions: {
+      input: resolve(__dirname, 'src/main.jsx')  // absolute path is safer
+    }
+  }
 })
