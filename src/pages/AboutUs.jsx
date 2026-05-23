217a12c6eea4abcfd5fd771632984d35f04d61ab
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-05-23 20:38:50 +0100

    Remove AboutFounder component from AboutUs
    Removed AboutFounder component from AboutUs page.

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -5,14 +5,26 @@ import Stats from "../components/Stats";
 import Wsa from "../components/Wsa";
 import Banner from "../components/BannerA";
 import Footer from "../components/Footer";
-import AboutFounder from "../components/AboutFounder";
 
 export default function AboutUs() {
   return (
     <div>
       <AboutHero />
+            <h1
+        style={{
+          textAlign: "center",
+          fontFamily: "Poppins, Inter, sans-serif",
+          fontSize: "1.5rem",
+          fontWeight: "700",
+          color: "#2c3e50",
+          marginTop: "40px",
+          marginBottom: "20px",
+          letterSpacing: "1px"
+        }}
+      >
+        CDL Training in Detroit, Michigan – Great Lakes CDL Academy
+      </h1>
       <Mission />
-      <AboutFounder />
       <Wcu />
       <Stats />
       <Wsa />
