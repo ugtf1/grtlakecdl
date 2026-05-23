f7f6ab34dcf991530c375d0a1fe5b37997bc39e8
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-05-23 20:39:22 +0100

    Add heading for CDL Training section in ContactUs

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,6 +6,20 @@ export default function ContactUs() {
   return (
     <div>
       <ContactHero />
+            <h1
+        style={{
+          textAlign: "center",
+          fontFamily: "Poppins, Inter, sans-serif",
+          fontSize: "1.5rem",
+          fontWeight: "700",
+          color: "#2c3e50",
+          marginTop: "40px",
+          marginBottom: "20px",
+          letterSpacing: "1px"
+        }}
+      >
+        CDL Training in Detroit, Michigan – Great Lakes CDL Academy
+      </h1>
       <Banner />
       <Footer />
     </div>
